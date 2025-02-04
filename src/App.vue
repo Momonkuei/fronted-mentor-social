a61442cbfaeca2251dbbe5a31989811bfc28424e
kuei <momonbin1991@icloud.com>
2025-02-04 10:24:43 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="bg-gray-900 h-screen w-screen flex justify-center items-center">
+    <card :data="data"></card>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import Card from './components/Card.vue'
+
+export default {
+  components: {
+    Card,
+  },
+  setup() {
+    // ...
+    const data = ref({
+      img: '/img/card/profile-pic.png',
+      name: 'Jessica Randall',
+      city: 'London, United Kingdom',
+      skill: 'Front-end developer and avid reader.',
+      links: [
+        {
+          name: 'GitHub',
+          link: 'javascript:void(0)',
+        },
+        {
+          name: 'Frontend Mentor',
+          link: 'javascript:void(0)',
+        },
+        {
+          name: 'LinkedIn',
+          link: 'javascript:void(0)',
+        },
+        {
+          name: 'Twitter',
+          link: 'javascript:void(0)',
+        },
+        {
+          name: 'Instagram',
+          link: 'javascript:void(0)',
+        },
+      ],
+    })
+
+    return {
+      data,
+    }
+  },
+}
+</script>
---
+++ src/components/Card.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="lg:p-[2.5rem] p-[1.5rem] bg-gray-800 w-[327px] md:w-[28.5rem] lg:w-[24rem] md:h-[611px] h-[579px] flex flex-col gap-[1.5rem] rounded-xl"
+  >
+    <div class="flex justify-center items-center">
+      <img class="max-w-[5.5rem] max-h-[5.5rem]" :src="data.img" alt="" />
+    </div>
+
+    <div class="text-center">
+      <div class="font-inter text-2xl leading-normal text-white font-bold">{{ data.name }}</div>
+      <div class="font-inter leading-normal text-sm text-design-green">{{ data.city }}</div>
+    </div>
+
+    <div class="text-center">
+      <div class="text-white leading-normal text-sm font-inter">"{{ data.skill }}"</div>
+    </div>
+
+    <div class="flex flex-col gap-[1rem]">
+      <a
+        v-for="list in data.links"
+        :href="list.link"
+        class="no-underline py-[0.75rem] bg-gray-700 text-white flex justify-center items-center hover:bg-design-green hover:text-gray-700 font-bold"
+        >{{ list.name }}</a
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['data'],
+  setup(props) {
+    console.log(props.data)
+  },
+}
+</script>
