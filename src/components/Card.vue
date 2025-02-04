be21255163cd03bd24a29d619d55a28598bbc5b5
kuei <momonbin1991@icloud.com>
2025-02-04 10:53:35 +0800

    20250204 更新

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,13 +1,14 @@
 <template>
   <div
     class="lg:p-[2.5rem] p-[1.5rem] bg-design-grey-800 w-[327px] md:w-[28.5rem] lg:w-[24rem] md:h-[611px] h-[579px] flex flex-col gap-[1.5rem] rounded-xl"
+    role="card"
   >
     <div class="flex justify-center items-center">
       <img class="max-w-[5.5rem] max-h-[5.5rem]" :src="data.img" alt="" />
     </div>
 
     <div class="text-center">
-      <div class="font-inter text-2xl leading-normal text-white font-bold">{{ data.name }}</div>
+      <h1 class="font-inter text-2xl leading-normal text-white font-bold">{{ data.name }}</h1>
       <div class="font-inter leading-normal text-sm text-design-green font-bold">
         {{ data.city }}
       </div>
@@ -21,6 +22,7 @@
       <a
         v-for="list in data.links"
         :href="list.link"
+        :key="list.name"
         class="no-underline py-[0.75rem] bg-design-grey-700 text-white flex justify-center items-center hover:bg-design-green hover:text-gray-700 font-bold"
         >{{ list.name }}</a
       >
